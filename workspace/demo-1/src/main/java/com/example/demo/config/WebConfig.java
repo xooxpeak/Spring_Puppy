fe35dacbbb9cc192e72ff32d80c278a8fdaf0064
xooxpeak <136714432+xooxpeak@users.noreply.github.com>
2024-02-14 22:46:24 +0900

    [Feat] CORS

---
+++ workspace/demo-1/src/main/java/com/example/demo/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+	
+	@Override
+    public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+//        .allowedOrigins("*")
+		.allowedOriginPatterns("*")
+        .allowedMethods("GET", "POST", "PUT", "DELETE")
+        .allowedHeaders("Authorization", "Content-Type")
+        .exposedHeaders("Custom-Header")
+        .allowCredentials(true);
+//        .maxAge(3600);
+    }
+
+}
