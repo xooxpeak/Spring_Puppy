10abecbb8cd4ebbe0b0a271896986df5c7586c4d
xooxpeak <136714432+xooxpeak@users.noreply.github.com>
2024-02-25 22:16:43 +0900

    [Feat] mapstruct 설정과 UserMapper 생성

---
+++ workspace/demo-1/src/main/java/com/example/demo/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package com.example.demo.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import com.example.demo.dto.UserDTO;
+import com.example.demo.entity.UserEntity;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+	UserMapper instance = Mappers.getMapper(UserMapper.class);
+	
+	UserEntity userEntityToUserDTO(UserDTO userDTO);
+}
