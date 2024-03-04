47886da5d7264e1df1b0421ccd38c285d173b184
xooxpeak <136714432+xooxpeak@users.noreply.github.com>
2024-03-04 20:41:18 +0900

    [Feat] 유효성 검사

--- workspace/demo-1/src/main/java/com/example/demo/controller/RegisterController.java
+++ workspace/demo-1/src/main/java/com/example/demo/controller/RegisterController.java
@@ -1,7 +1,9 @@
 package com.example.demo.controller;
 
 import com.example.demo.dto.ResDTO;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dto.UserDTO;
-import com.example.demo.entity.UserEntity;
 import com.example.demo.service.UserService;
 
 @RestController
@@ -23,13 +24,23 @@ public class RegisterController {
 	/**
 	기능 : 회원가입
 	url : /register
-	request Data : 아이디, 비밀번호, 비밀번호 확인, 이메일, 이름, 생년월일, 주소
+	request Data : 아이디, 비밀번호, 비밀번호2, 이메일, 이름, 생년월일, 주소
 	Response Data : 이메일 인증, 회원가입 성공
 	 */
 	@PostMapping("/register")
-	public ResDTO register(@RequestBody UserDTO userDTO) {
+	public ResDTO register(@Valid @RequestBody UserDTO userDTO, Errors errors) {
 		// TODO: 유효성 검사 관련 내용 추가
-		return userService.register(userDTO);
+		// 유효성 검사 결과 확인
+		if (errors.hasErrors()) {
+			// 유효성 검사 실패 시 처리
+			return ResDTO.builder()
+					.code("402")
+					.message("입력값이 유효하지 않습니다.")
+					.data(errors.getAllErrors())
+					.build();
+		} else {
+				return userService.register(userDTO);
+			}
 	}
 	
 	
@@ -51,7 +62,7 @@ public class RegisterController {
 	url : /dupIdCheck
 	request Data : 아이디
 	Response Data : 사용 가능한 아이디 or 중복된 아이디
-	 * @throws Exception 
+	 * @throws Exception
 	 */
 	@PostMapping("/dupIdCheck")
 	public Object dupIdCheck(@RequestParam(value = "userId") String userId) throws Exception {
--- workspace/demo-1/src/main/java/com/example/demo/dto/UserDTO.java
+++ workspace/demo-1/src/main/java/com/example/demo/dto/UserDTO.java
@@ -2,6 +2,8 @@ package com.example.demo.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,20 +18,32 @@ import lombok.Setter;
 public class UserDTO {
 	
 	private Long id;
-	
+
 	@JsonProperty("userId")
+	@NotBlank(message = "아이디는 필수 입력 값입니다.")
 	private String userId;
 	
 	@JsonProperty("password")
+	@NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+	@Pattern(regexp = "(?=.*[a-z])(?=.*[0-9]).{8,16}$", message = "비밀번호는 8~16자 영문 소문자, 숫자를 조합하여 입력하세요.")
 	private String password;
+
+	@JsonProperty("password2")
+	@NotBlank(message = "비밀번호는 필수 입력 값입니다.")
+	@Pattern(regexp = "(?=.*[a-z])(?=.*[0-9]).{8,16}$", message = "비밀번호는 8~16자 영문 소문자, 숫자를 조합하여 입력하세요.")
+	private String password2;
 	
 	@JsonProperty("name")
+	@NotBlank(message = "이름은 필수 입력 값입니다.")
 	private String name;
 	
 	@JsonProperty("birth")
+	@NotBlank(message = "생년월일은 필수 입력 값입니다.")
 	private String birth;
 	
 	@JsonProperty("email")
+	@NotBlank(message = "이메일은 필수 입력 값입니다.")
+	@Pattern(regexp = "^(?:\\w+\\.?)*\\w+@(?:\\w+\\.)+\\w+$", message = "이메일 형식이 올바르지 않습니다.")
 	private String email;
 	
 	@JsonProperty("addr1")
