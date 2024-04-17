7a71f83f4dfe780360a1fb584f00908409171b46
xooxpeak <136714432+xooxpeak@users.noreply.github.com>
2024-04-17 12:31:02 +0900

    [Feat] 사진첩 작성 컨트롤러

--- workspace/demo-1/src/main/java/com/example/demo/controller/GalleryController.java
+++ workspace/demo-1/src/main/java/com/example/demo/controller/GalleryController.java
@@ -1,26 +1,83 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.dto.UploadResDTO;
+import com.example.demo.service.GalleryService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
-
+@Log4j2
+@RequestMapping("/api/v1/auth/y")
 public class GalleryController {
 
+	@Autowired
+	GalleryService galleryService;
+
+//	// 업로드 할 위치
+//	@Value("${part.upload.path}")
+//	private String uploadPath;
+
 	/**
-	  기능 : 사진첩 작성  ( 훈련사 계정 )
-	  url : /createGallery
-	  request data : 날짜(글제목), 글 내용, 작성 날짜, 작성 시간
-	  response data : 사진첩 작성 성공
+	 * 기능 : 사진첩 작성  ( 훈련사 계정 )
+	 * url : /createGallery
+	 * request data : 날짜(글제목), 글 내용, 작성 날짜, 작성 시간
+	 * response data : 사진첩 작성 성공
 	 */
-	
 	@PostMapping("/createGallery")
-	public Object createGallery() {
-		return null;
+	public List<UploadResDTO> createGallery(@RequestParam("uploadFiles") MultipartFile[] uploadFiles) {
+		return galleryService.createGallery(uploadFiles);
 	}
+//	public ResponseEntity<List<UploadResDTO>> createGallery(MultipartFile[] uploadFiles) {
+//
+//		List<UploadResDTO> uploadResDTOList = new ArrayList<>();
+//
+//		// 업로드 할 위치
+////		String uploadPath = "C:/Users/xooxpeak/Desktop/과외/Upload";
+//
+//		for (MultipartFile uploadFile: uploadFiles) {
+//
+//			// 이미지 파일만 업로드
+//			if (!Objects.requireNonNull(uploadFile.getContentType()).startsWith("image")) {
+//				log.warn("this file is not image type");
+//				return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+//			}
+//
+//			// 파일명: 모든 경로를 포함한 파일이름
+//			String originalName = uploadFile.getOriginalFilename();
+//			// 원본 파일 이름이 null이 아닌지 확인
+//			assert originalName != null;
+//			// 마지막으로온 "/"부분으로부터 +1 해준 부분부터 출력
+//			String fileName = originalName.substring(originalName.lastIndexOf("//")+1);
+//			log.info("fileName" + fileName);
+//
+//			//날짜 폴더 생성
+//			String folderPath = galleryService.makeFolder();
+//			//UUID
+//			String uuid = UUID.randomUUID().toString();
+//			//저장할 파일 이름 중간에 "_"를 이용하여 구분
+//			String saveName = uploadPath + File.separator + folderPath +File.separator + uuid + "_" + fileName;
+//
+//			//Paths.get() 메서드는 특정 경로의 파일 정보를 가져옵니다.(경로 정의하기)
+//			Path savePath = Paths.get(saveName);
+//
+//			try{
+//				// transferTo(file) : uploadFile에 파일을 업로드 하는 메서드
+//				uploadFile.transferTo(savePath);
+//				uploadResDTOList.add(new UploadResDTO(fileName, uuid, folderPath));
+//			} catch (IOException e) {
+//				e.printStackTrace();
+//			}
+//        }
+//		// 모든 파일 업로드가 성공한 경우
+//		return new ResponseEntity<>(uploadResDTOList, HttpStatus.OK);
+//
+//	}
+
+
 	
 	
 	/**
