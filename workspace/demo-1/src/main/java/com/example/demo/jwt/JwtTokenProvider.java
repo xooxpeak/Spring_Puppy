0a371ed1ad0294b98d27d0e9f60dacc5e135ac64
xooxpeak <136714432+xooxpeak@users.noreply.github.com>
2024-04-20 18:00:00 +0900

    [Add] 토큰을 복호화하여 토큰에 들어있는 정보를 꺼내는 메서드 추가

--- workspace/demo-1/src/main/java/com/example/demo/jwt/JwtTokenProvider.java
+++ workspace/demo-1/src/main/java/com/example/demo/jwt/JwtTokenProvider.java
@@ -5,14 +5,17 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
 
 import java.security.Key;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -64,38 +67,38 @@ public class JwtTokenProvider {
     }
 
     // JWT 토큰을 복호화하여 토큰에 들어있는 정보를 꺼내는 메서드
-//    public Authentication getAuthentication(String accessToken) {
-//        // 토큰 복호화
-//        Claims claims = parseClaims(accessToken);
-//
-//        if (claims.get("auth") == null) {
-//            throw new RuntimeException("권한 정보가 없는 토큰입니다.");
-//        }
-//
-//        // 클레임에서 권한 정보 가져오기
-//        Collection<? extends GrantedAuthority> authorities =
-//                Arrays.stream(claims.get("auth").toString().split(","))
-//                        .map(SimpleGrantedAuthority::new)
-//                        .collect(Collectors.toList());
-//
-//        // UserDetails 객체를 만들어서 Authentication 리턴
-//        UserDetails principal = new User(claims.getSubject(), "", authorities);
-//        return new UsernamePasswordAuthenticationToken(principal, "", authorities);
-//    }
+    public Authentication getAuthentication(String accessToken) {
+        // 토큰 복호화
+        Claims claims = parseClaims(accessToken);
+
+        if (claims.get("auth") == null) {
+            throw new RuntimeException("권한 정보가 없는 토큰입니다.");
+        }
+
+        // 클레임에서 권한 정보 가져오기
+        Collection<? extends GrantedAuthority> authorities =
+                Arrays.stream(claims.get("auth").toString().split(","))
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toList());
+
+        // UserDetails 객체를 만들어서 Authentication 리턴
+        UserDetails principal = new User(claims.getSubject(), "", authorities);
+        return new UsernamePasswordAuthenticationToken(principal, "", authorities);
+    }
 
     // 토큰 정보를 검증하는 메서드
     public boolean validateToken(String token) {
         try {
-            Jwts.parserBuilder()
-                    .setSigningKey(key)
+            Jwts.parserBuilder()    // JWT를 파싱하고 검증하는 빌더 객체 생성
+                    .setSigningKey(key)   // 해당 토큰을 생성할 때 사용된 키를 설정
                     .build()
-                    .parseClaimsJws(token);
-            return true;
-        } catch (SecurityException | MalformedJwtException e) {
+                    .parseClaimsJws(token);   // 해당 토큰을 파싱하고 서명을 검증
+            return true;    // 성공 시 -> 유효한 토큰임 -> true 반환
+        } catch (SecurityException | MalformedJwtException e) {   // 잘못된 형식인 경우
             log.info("Invalid JWT Token", e);
-        } catch (ExpiredJwtException e) {
+        } catch (ExpiredJwtException e) {   // 토큰이 만료된 경우
             log.info("Expired JWT Token", e);
-        } catch (UnsupportedJwtException e) {
+        } catch (UnsupportedJwtException e) {    // 지원되지 않는 토큰인 경우
             log.info("Unsupported JWT Token", e);
         } catch (IllegalArgumentException e) {
             log.info("JWT claims string is empty.", e);
