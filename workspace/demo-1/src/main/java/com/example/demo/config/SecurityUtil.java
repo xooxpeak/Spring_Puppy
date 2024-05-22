cfcf735c3c0282e452ff1a1a4c5267a839eeed55
xooxpeak <136714432+xooxpeak@users.noreply.github.com>
2024-05-22 17:59:46 +0900

    [Add] 현재 사용자의 인증 및 권한 정보를 가져오기 위한 SecurityUtil 클래스 생성

---
+++ workspace/demo-1/src/main/java/com/example/demo/config/SecurityUtil.java
@@ -0,0 +1,21 @@
+package com.example.demo.config;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class SecurityUtil {
+
+    public static String getCurrentUserId() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.isAuthenticated()) {
+            Object principal = authentication.getPrincipal();
+            if (principal instanceof UserDetails) {
+                return ((UserDetails) principal).getUsername();
+            } else {
+                return principal.toString();
+            }
+        }
+        return null;
+    }
+}
